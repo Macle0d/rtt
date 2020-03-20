894d4d0f66ceee085317fb1ff2ea3f92c21cbed3
Omar Peña <macleod@localhost.localdomain>
2020-03-20 17:20:50 -0300

    Agrego scanner SMBGhost

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo""
 if [ "$1" = "all" ]; then
     sudo cp -fv scan-ms17-010.py /usr/bin/
     sudo cp -fv nlocate /usr/bin/
+    sudo cp -fv scan-SMBGhost.py /usr/bin/
 
 elif [ "$1" = "scan-ms17-010.py" ]; then
     sudo cp -fv scan-ms17-010.py /usr/bin/
@@ -10,9 +11,13 @@ elif [ "$1" = "scan-ms17-010.py" ]; then
 elif [ "$1" = "nlocate" ]; then
     sudo cp -fv nlocate /usr/bin/
 
+elif [ "$1" = "scan-SMBGhost.py" ]; then
+    sudo cp -fv scan-SMBGhost.py /usr/bin/
+
 else
     echo "Usage:
     sh install.sh nlocate
     sh install.sh scan-ms17-010.py
+    sh install.sh scan-SMBGhost.py
     sh install.sh all\n"
 fi
